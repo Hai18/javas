008e5494c9ff2c8ab53560f46931b67d686b802f
Hay <Hay@PushMyWeb-iMac.local>
2017-06-23 11:27:06 -0700

    this is the javas we talked about

---
+++ script.js
@@ -0,0 +1,19 @@
+var hour = 8;
+var minute = 50;
+var period = "AM";
+
+if (hour == 8 && minute <= 50 && period == "AM")
+{
+
+    console.log("Its almost 9 in the morning");
+
+}
+var hour = 7;
+var minute = 15;
+var period = "PM";
+
+if (hour == 7 && minute == 15 && period == "PM"){
+
+    console.log("Its just after 7 in the evening");
+}
+console.log("blah blhab");
